1c2726cbbb0a65700cc0127a0abee313a19e7b36
Yugyeong Park <bonbonly7@naver.com>
2023-08-20 02:56:46 +0900

    TMAP API URL의 상수 이름 변경

--- app/src/main/java/com/soundexpedition/blindguide/core/Setting.kt
+++ app/src/main/java/com/soundexpedition/blindguide/core/Setting.kt
@@ -1,6 +1,6 @@
 package core
 
 object Setting {
-    const val TMAP_URL = "https://apis.openapi.sk.com/tmap/routes/pedestrian?version={version}&callback={callback}"
-    const val TMAP_API = "CJr9vILJSc5qEIpXm3ouh3PwtJNxYqtQyNeB9cPe"
+    const val TMAP_URL = "https://apis.openapi.sk.com/"
+    const val TMAP_KEY = "CJr9vILJSc5qEIpXm3ouh3PwtJNxYqtQyNeB9cPe"
 }
