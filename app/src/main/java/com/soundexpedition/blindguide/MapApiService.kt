70ee131c5291578942a13771b989407ca8b6e4d3
Yugyeong Park <bonbonly7@naver.com>
2023-08-20 03:51:55 +0900

    - "accept" Header 추가 - GET 요청 삭제 - POST 요청 경로 변경 - "version" Query 삭제 후 POST 요청 경로에 삽입

--- app/src/main/java/com/soundexpedition/blindguide/MapApiService.kt
+++ app/src/main/java/com/soundexpedition/blindguide/MapApiService.kt
@@ -1,34 +1,29 @@
 package com.soundexpedition.blindguide
 
-import core.*
-import model.*
-import retrofit2.http.GET
+import com.soundexpedition.blindguide.core.*
+import com.soundexpedition.blindguide.model.*
 import retrofit2.http.POST
 import retrofit2.http.Header
 import retrofit2.http.Query
 import retrofit2.http.Body
 import retrofit2.Response
+import retrofit2.http.FormUrlEncoded
 
+//postman
 interface MapApiService {
-    @GET(Setting.TMAP_URL)
-    suspend fun getResponseInfo(
-        @Header("appKey") appKey: String = Setting.TMAP_API,
+    //@FormUrlEncoded
+    @POST("tmap/routes/pedestrian?version=1")
+    suspend fun postResponseInfo(
+        @Header("accept") accept: String = "application/json",
+        @Header("appKey") appKey: String = Setting.TMAP_KEY,
 
         //Querystring Parameters
-        @Query("version") version: Int = 1,
-        @Query("callback") callback: String? = null,
-
-        ): Response<ResponseInfo>
-
-    @POST(Setting.TMAP_URL)
-    suspend fun postResponseInfo(
-        @Header("appKey") appKey: String = Setting.TMAP_API,
+        //@Query("version") version: Int = 1,
 
         // Request Payload
         @Body requestPayload: RequestPayload
     ): Response<ResponseInfo>
 }
 
-//@GET은 HTTP 통신 방식으로 Parameter들이 URL에 추가되는 GET 방식으로 받아오겠다.
 //@Header는 Request Header에 TMAP_API의 Key값을 추가한다.
 //@Query는 Request시 담을 Parameter들이며 아래와 같이 URL이 구성되며 Request를 보낸다.
